ca0c0f04b43ad093df612dbe4a63ac92164100af
Sarah Bernal <bernals1@usf.edu>
2025-10-26 22:50:16 -0400

    set default counts

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,8 @@ export default function Home() {
   return (
     <main style={{ textAlign: "center", padding: "40px" }}>
       <h1>Next.js Button Counter</h1>
-
-        {/* First counter with default values */}
-      <Counter />
-
-      {/*Second counter with custom starting values */}
-      <Counter initialCount={10} initialStep={5} />
+      <Counter /> {/* default count=0, step=1 */}
+      <Counter initialCount={5} initialStep={2} /> {/* custom props */}
     </main>
   );
 }
